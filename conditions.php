dbf7e9db018777585b452b98464b9144ee2a33d8
ddvdv <vdv.david@gmail.com>
2017-05-02 14:36:31 +0200

    Ajout gestion de l'âge

--- conditions.php
+++ conditions.php
@@ -4,6 +4,31 @@
 
 	<body>
 
+	<form method="get">
+		<input name="age" placeholder="Quel est ton âge?">
+		<input type="submit" value="submit">
+	</form>
+
+	<?php
+
+	if (isset($_GET['age'])) {
+	$age = $_GET['age'];
+		if ($age < 12) {
+			echo "Salut petit!";
+		}
+		elseif ($age > 11 AND $age < 19) {
+			echo "Salut ado!";
+		}
+		elseif ($age > 18 AND $age < 115) {
+			echo "Salut l'adulte";
+		}
+		elseif ($age > 114) {
+			echo "Wow. Toujours en vie?";
+		}
+
+	}
+	?>
+
 	<?php
 	/*	Si l'heure est entre 05h00 et 9h00 du matin, affiche "Bonjour!".
 	Si l'heure est entre 9h01 et 12h00, affiche "Bonne journée!".
